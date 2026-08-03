e0f0a444cdd9bfd43be09d5b8145da0570b5c7e0
Arjun <arjunsinghanya35@gmail.com>
2026-08-03 14:37:16 +0530

    banner image added.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,23 @@
-<h1>Hello</h1>
+@extends('layouts.app')
+
+@section('meta')
+    <title>About SHABDD Travel</title>
+    <meta name="description"
+        content="Learn about SHABDD Travel and our approach to creating curated holiday experiences.">
+@endsection
+
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/about.css') }}">
+@endpush
+
+
+@section('content')
+    <main class="about-page">
+        <div class="about-image-banner">
+            <img src="/about-image/about-banner.png" alt="About SHABDD Travel">
+            
+        </div>
+        
+
+    </main>
+@endsection
